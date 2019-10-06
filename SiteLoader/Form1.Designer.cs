680f3b63a546d3d12cb2aa0145f45a4b40ae3d77
kassadube <noamsrb@yahoo.com>
2019-10-06 14:44:38 +0300

    complete

--- SiteLoader/Form1.Designer.cs
+++ SiteLoader/Form1.Designer.cs
@@ -28,34 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.normalbrn = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ParallelAsync = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // normalbrn
             // 
-            this.button1.Location = new System.Drawing.Point(27, 41);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.normalbrn.Location = new System.Drawing.Point(27, 41);
+            this.normalbrn.Name = "normalbrn";
+            this.normalbrn.Size = new System.Drawing.Size(512, 23);
+            this.normalbrn.TabIndex = 0;
+            this.normalbrn.Text = "normal";
+            this.normalbrn.UseVisualStyleBackColor = true;
+            this.normalbrn.Click += new System.EventHandler(this.Normal_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(63, 233);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(683, 97);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 388);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(683, 130);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(512, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Async";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Async_Click);
+            // 
+            // ParallelAsync
+            // 
+            this.ParallelAsync.Location = new System.Drawing.Point(27, 100);
+            this.ParallelAsync.Name = "ParallelAsync";
+            this.ParallelAsync.Size = new System.Drawing.Size(512, 23);
+            this.ParallelAsync.TabIndex = 5;
+            this.ParallelAsync.Text = "Parralel Async";
+            this.ParallelAsync.UseVisualStyleBackColor = true;
+            this.ParallelAsync.Click += new System.EventHandler(this.ParallelAsync_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(876, 584);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ParallelAsync);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.normalbrn);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button normalbrn;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button ParallelAsync;
     }
 }
 
--- SiteLoader/Form1.cs
+++ SiteLoader/Form1.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,9 +20,134 @@ namespace SiteLoader
             InitializeComponent();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void Normal_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("sdfsdfsdf");
+            textBox1.Clear();
+            var watch = Stopwatch.StartNew();
+            RunDownloadSync();
+            watch.Stop();
+            var elspms = watch.ElapsedMilliseconds;
+
+            textBox1.AppendText($"Total Execution {elspms}");
+
         }
+
+        private async void Async_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            var watch = Stopwatch.StartNew();
+            await RunDownloadAsync();
+            watch.Stop();
+            var elspms = watch.ElapsedMilliseconds;
+
+            textBox1.AppendText($"Total Execution {elspms}");
+        }
+
+        private async void ParallelAsync_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            var watch = Stopwatch.StartNew();
+            await RunDownloadParallelAsync();
+            watch.Stop();
+            var elspms = watch.ElapsedMilliseconds;
+
+            textBox1.AppendText($"Total Execution {elspms}");
+        }
+
+        private void RunDownloadSync()
+        {
+            List<string> list = PrepareData();
+            foreach (var site in list)
+            {
+                WebsiteDataModel model = DownloadSite(site);
+                ReportWebsiteInfo(model);
+            }
+        }
+
+        private async Task RunDownloadAsync()
+        {
+            List<string> list = PrepareData();
+            foreach (var site in list)
+            {
+                WebsiteDataModel model = await Task.Run(() => DownloadSite(site));
+                ReportWebsiteInfo(model);
+            }            
+        }
+
+        private async Task RunDownloadParallelAsync()
+        {
+            List<string> list = PrepareData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (var site in list)
+            {
+                tasks.Add(DownloadSiteAsync(site));              
+            }
+            var results = await Task.WhenAll(tasks);
+            
+            foreach (var item in results)
+            {
+                ReportWebsiteInfo(item);
+            }
+        }
+        /*
+        private void RunDownloadParallelAsync()
+        {
+            List<string> list = PrepareData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (var site in list)
+            {
+                DownloadSiteAsync(site).ContinueWith((m) => ReportWebsiteInfo(m.Result), TaskScheduler.FromCurrentSynchronizationContext());
+                // tasks.Add());               
+            }
+            // var results = await Task.WhenAll(tasks);
+
+            //foreach (var item in results)
+            //{
+            //    ReportWebsiteInfo(item);
+            //}
+        }
+        */
+        private WebsiteDataModel DownloadSite(string url)
+        {
+            WebsiteDataModel res = new WebsiteDataModel();
+
+            WebClient client = new WebClient();
+            res.WebsiteUrl = url;
+            res.WebsiteData = client.DownloadString(url);
+            //Thread.Sleep(1000);
+
+            return res;
+        }
+
+        private async Task<WebsiteDataModel> DownloadSiteAsync(string url)
+        {
+            WebsiteDataModel res = new WebsiteDataModel();
+
+            WebClient client = new WebClient();
+            res.WebsiteUrl = url;
+            res.WebsiteData = await client.DownloadStringTaskAsync(url);
+            //Thread.Sleep(1000);
+
+            return res;
+        }
+        private List<string> PrepareData()
+        {
+            List<string> list = new List<string>();
+            list.Add("https://www.msn.com");
+            list.Add("https://www.google.com");
+            list.Add("https://www.cnn.com");
+            list.Add("https://www.microsoft.com");
+            list.Add("https://www.yahoo.com");
+            list.Add("https://www.foxnews.com");
+            
+            return list;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            textBox1.AppendText($"{data.WebsiteUrl} downloaded: {data.WebsiteData.Length} charecters.{Environment.NewLine}");
+        }
+
+        
     }
 }
